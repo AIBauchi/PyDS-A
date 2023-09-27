b1713cc43121f7dd1c56ecdcb8f57d50698728f2
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2023-09-27 21:38:14 +0000

    error

--- test/test_data_structures/test_linked_list.py
+++ test/test_data_structures/test_linked_list.py
@@ -2,41 +2,41 @@ import pytest
 from pyds_a.data_structures.linked_list import LinkedList  # Import your LinkedList implementation
 
 def test_empty_linked_list():
-    ll = LinkedList()
-    assert ll.is_empty() is True
-    assert ll.size() == 0
+    test_list = LinkedList()
+    assert test_list.is_empty() is True
+    assert test_list.size() == 0
 
 def test_linked_list_insert():
-    ll = LinkedList()
-    ll.insert(1)
-    assert ll.size() == 1
-    assert ll.search(1) is True
+    test_list = LinkedList()
+    test_list.insert(1)
+    assert test_list.size() == 1
+    assert test_list.search(1) is True
 
 def test_linked_list_delete():
-    ll = LinkedList()
-    ll.insert(1)
-    ll.insert(2)
-    ll.delete(1)
-    assert ll.size() == 1
-    assert ll.search(1) is False
-    assert ll.search(2) is True
+    test_list = LinkedList()
+    test_list.insert(1)
+    test_list.insert(2)
+    test_list.delete(1)
+    assert test_list.size() == 1
+    assert test_list.search(1) is False
+    assert test_list.search(2) is True
 
 def test_linked_list_get_list():
-    ll = LinkedList()
-    ll.insert(1)
-    ll.insert(2)
-    ll.insert(3)
-    assert ll.get_list() == [1, 2, 3]
+    test_list = LinkedList()
+    test_list.insert(1)
+    test_list.insert(2)
+    test_list.insert(3)
+    assert test_list.get_list() == [1, 2, 3]
 
 def test_linked_list_clear():
-    ll = LinkedList()
-    ll.insert(1)
-    ll.insert(2)
-    ll.clear()
-    assert ll.size() == 0
-    assert ll.is_empty() is True
-    assert ll.search(1) is False
-    assert ll.search(2) is False
+    test_list = LinkedList()
+    test_list.insert(1)
+    test_list.insert(2)
+    test_list.clear()
+    assert test_list.size() == 0
+    assert test_list.is_empty() is True
+    assert test_list.search(1) is False
+    assert test_list.search(2) is False
 
 if __name__ == "__main__":
     pytest.main()
