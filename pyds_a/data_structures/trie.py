72a93e88158a638da11ab5505d81cd5d27025f92
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-10-15 13:36:30 +0530

    Update trie.py
    changes file structure of trie.py makes it analogous to other files in the repo.

--- pyds_a/data_structures/trie.py
+++ pyds_a/data_structures/trie.py
@@ -7,7 +7,7 @@ Tries also known as Prefix-Trees are a subcase of Trees which are used specially
 strings and makes the time complexity for searching a word O(n), where n is the length of the
 word to be searched
 
-The trie class provides methods for adding words to the trie, checking whether a spcific
+The Trie class provides methods for adding words to the trie, checking whether a spcific
 word is present in the trie, and to see whether the words in the trie start from a specific 
 prefix/substring.
 
@@ -28,18 +28,33 @@ Usage:
 
 
 class Trie:
+    """
+        Represents a trie data structure implemented using a dictionary.
+        
+        A trie has a tree structure and stores string(s) by breaking them into charectars and storing them according 
+        to their keys, here keys refers to the prefixes or the word which apear before any given charectar in the same 
+        sequence as they appear in the word to be stored. Each charectar is connected to the one which appears before it in the word.
+        
+        Methods:
+            
+            insert(word): Inserts a word in the trie.
+            search_word(sord): Searches for a word in the trie
+            starts_with(substring): Searches for a substring in the begining of the words in the trie.
+                 
+     """
 
     def __init__(self):
         """
-        Initialises trie data structure with an empty dictionary and a variable to store Boolean values.
+        Initialises  an empty dictionary and a variable to store Boolean values.
         """
+
       self.children = {}
       self.last_letter = False
 
 
     def insert(self, word):
         """
-        Inserts a word in the trie data structure
+        Inserts a word in the trie data structure.
 
         Args :
             word (string): The word to be added                
