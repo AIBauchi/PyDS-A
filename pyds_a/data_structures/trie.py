eef820d0daf4081b7fd52c0359c90920884df10e
rghvsh <116428320+rghvsh@users.noreply.github.com>
2023-10-16 13:07:24 +0530

    Update trie.py
    Corrected indentation.

--- pyds_a/data_structures/trie.py
+++ pyds_a/data_structures/trie.py
@@ -1,5 +1,4 @@
-"""
-Author: Rarghav Sharma(rghvsh)
+"""Author: Rarghav Sharma(rghvsh)
 trie.py - A Python implementation of a trie data structure.
 
 This file contains the implementation of a trie data structure using a OOP and dictionary.
@@ -41,24 +40,25 @@ class Trie:
             search_word(sord): Searches for a word in the trie
             starts_with(substring): Searches for a substring in the begining of the words in the trie.
                  
-     """
+    """
 
     def __init__(self):
-        """
+      """
         Initialises  an empty dictionary and a variable to store Boolean values.
-        """
+      """
 
       self.children = {}
       self.last_letter = False
 
 
     def insert(self, word):
-        """
+        
+      """
         Inserts a word in the trie data structure.
 
         Args :
             word (string): The word to be added                
-        """
+      """
       root = self
       for char in word:
         if char not in root.children:
@@ -70,7 +70,7 @@ class Trie:
 
 
     def search_word(self, word):
-        """
+      """
         Searches for a word in the trie data structure.
 
         Args:
@@ -78,7 +78,7 @@ class Trie:
 
         Returns:
             bool : Returns a boolean value whether the word is present in the trie.
-        """
+      """
       root = self
       for char in word:
         if char not in root.children:
@@ -90,7 +90,7 @@ class Trie:
 
 
     def starts_with(self, substring):
-         """
+      """
         Searches whether there are words in the trie which start with the given substring.
 
         Args:
@@ -98,7 +98,7 @@ class Trie:
 
         Returns:
             bool : Returns a boolean value whether there are words in the trie which begin with the given substring.
-        """
+      """
       root = self
       for char in substring:
         if char not in root.children:
